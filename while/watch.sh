9a3138a044105960ad60f1cdd9458eb94bca6e5a
Alexander Tarasenko <tarasenkoas@gmail.com>
2023-11-13 13:41:43 +0300

    init

---
+++ while/watch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+while :; do
+  mv -f b.json a.json
+  curl -s "https://cash.rbc.ru/cash/json/cash_rates_with_volumes/?city=1&currency=3&_=$(date +%s)000" \
+    | jq --indent 0 '.banks[] | [ .name, .id, .rate.volume_id, .rate.sell ]' \
+    | sort > b.json
+  date
+  diff a.json b.json
+  sleep 60
+done
+
